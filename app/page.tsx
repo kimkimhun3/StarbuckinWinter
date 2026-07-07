f4c1b47766be881e73caebf26ea56f118d40d0c4
Kimhun <hoon@Kimhuns-MacBook-Pro.local>
2026-07-07 23:55:45 +0900

    Intro change

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function HomePage() {
   return (
     <main className="bg-[#F5F1E8]">
       <Hero />
-      <IntroSection />
       <HousesSection />
+      <IntroSection />
       <ProfilesSection />
       <MagazineSection />
       <SchoolSection />
